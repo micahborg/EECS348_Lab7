6d2d7357356cb51b672da56fd3823420b5ce6699
Micah Borghese <micahborghese@gmail.com>
2023-04-06 23:29:13 -0500

    Comments and HTML Styling

--- practice4.php
+++ practice4.php
@@ -1,3 +1,7 @@
 <?php
+/*
+* Create a PHP program that receives a number and displays a multiplication table from 1 to the
+* number. You also need to print the row and column indexes in the table.
+*/
 echo "hello world!";
 ?>
