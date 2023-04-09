7eab803294dc0c67bfc810524da57569b267958e
Micah Borghese <micahborghese@gmail.com>
2023-04-09 18:14:51 -0500

    Updated files

--- practice4.php
+++ practice4.php
@@ -1,7 +1,26 @@
 <?php
 /*
-* Create a PHP program that receives a number and displays a multiplication table from 1 to the
+* Create a PHP program that receives a size and displays a multiplication table from 1 to the
 * number. You also need to print the row and column indexes in the table.
 */
-echo "hello world!";
+$size = $_POST['size'];
+
+echo "<table border='1'>";
+for ($i = 0; $i <= $size; $i++) {
+    echo "<tr>";
+    for ($j = 0; $j <= $size; $j++) {
+        if ($i == 0 && $j == 0) {
+            echo "<td></td>";
+        } else if ($i == 0) {
+            echo "<td><b>$j</b></td>";
+        } else if ($j == 0) {
+            echo "<td><b>$i</b></td>";
+        } else {
+            echo "<td>" . $i * $j . "</td>";
+        }
+    }
+    echo "</tr>";
+}
+echo "</table>";
+
 ?>
